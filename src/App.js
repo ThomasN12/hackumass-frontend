2e68bc4abc041267139faa6945eff0dccbbb0817
ThomasN12 <phuocthinhdn12023@gmail.com>
2022-11-12 00:49:07 -0500

    add Layout + MainNavigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
+import Layout from './components/layout/Layout';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-      <h1>Temp</h1>
-      // <Routes>
-      //   <Route path="/" element={<HomePage />} />
-      //   <Route path="/campaigns" element={<AllCampaigns />} />
-      //   <Route path="/our-team" element={<EBoardMembers />} />
-      //   <Route path="*" element={<NotFound />} />
-      // </Routes>
+    <Layout>
+      <Routes>
+        {/* <Route path="/" element={<HomePage />} />
+        <Route path="/campaigns" element={<AllCampaigns />} />
+        <Route path="/our-team" element={<EBoardMembers />} />
+        <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </Layout>
+
   );
 }
 
