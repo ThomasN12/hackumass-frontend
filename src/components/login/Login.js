528c704e5bc85756571b04cc65df0c2d2297df24
laithiennhan <ntlai@umass.edu>
2022-11-12 01:44:12 -0500

    login page and css

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,18 +1,81 @@
-import "./login.css"
-import React from "react"
 
-const Login = (props) => {
+import React, { useState } from "react"
+import "./login.css"
+
+export default function (props) {
+  let [authMode, setAuthMode] = useState("signin")
+
+  const changeAuthMode = () => {
+    setAuthMode(authMode === "signin" ? "signup" : "signin")
+  }
+
+  if (authMode === "signin") {
+    return (
+      <div className="Auth-form-container">
+        <form className="Auth-form">
+          <div className="Auth-form-content">
+            <h3 className="Auth-form-title">Sign In</h3>
+            <div className="text-center">
+              Not registered yet?{" "}
+              <span className="link-primary option" onClick={changeAuthMode}>
+                Sign Up
+              </span>
+            </div>
+            <div className="form-group mt-3">
+              <label>Email address</label>
+              <input
+                type="email"
+                className="form-control mt-1"
+                placeholder="Enter email"
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Enter password"
+              />
+            </div>
+            <div className="d-grid gap-2 mt-3">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+            <p className="text-center mt-2">
+              Forgot <a href="#">password?</a>
+            </p>
+          </div>
+        </form>
+      </div>
+    )
+  }
+
   return (
     <div className="Auth-form-container">
       <form className="Auth-form">
         <div className="Auth-form-content">
           <h3 className="Auth-form-title">Sign In</h3>
+          <div className="text-center">
+            Already registered?{" "}
+            <span className="link-primary" onClick={changeAuthMode}>
+              Sign In
+            </span>
+          </div>
+          <div className="form-group mt-3">
+            <label>Full Name</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="e.g Jane Doe"
+            />
+          </div>
           <div className="form-group mt-3">
             <label>Email address</label>
             <input
               type="email"
               className="form-control mt-1"
-              placeholder="Enter email"
+              placeholder="Email Address"
             />
           </div>
           <div className="form-group mt-3">
@@ -20,7 +83,7 @@ const Login = (props) => {
             <input
               type="password"
               className="form-control mt-1"
-              placeholder="Enter password"
+              placeholder="Password"
             />
           </div>
           <div className="d-grid gap-2 mt-3">
@@ -28,7 +91,7 @@ const Login = (props) => {
               Submit
             </button>
           </div>
-          <p className="forgot-password text-right mt-2">
+          <p className="text-center mt-2">
             Forgot <a href="#">password?</a>
           </p>
         </div>
@@ -36,5 +99,3 @@ const Login = (props) => {
     </div>
   )
 }
-
-export default Login
