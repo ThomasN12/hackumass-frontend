fcd216ad42f830a3f38caaf2d34d21717023ebdd
laithiennhan <ntlai@umass.edu>
2022-11-12 01:25:37 -0500

    fix bootstrap error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import Layout from './components/layout/Layout';
 import { Route, Routes } from "react-router-dom";
+import Login from './components/login/Login.js';
+import "bootstrap/dist/css/bootstrap.css";
+
 
 function App() {
   return (
@@ -9,7 +12,9 @@ function App() {
         {/* <Route path="/" element={<HomePage />} />
         <Route path="/campaigns" element={<AllCampaigns />} />
         <Route path="/our-team" element={<EBoardMembers />} />
-        <Route path="*" element={<NotFound />} /> */}
+        // <Route path="*" element={<NotFound />} /> */
+        <Route path='/' element={<Login/>}/>
+        }
       </Routes>
     </Layout>
 
