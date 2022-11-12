e2bc8c098530b0e2ec3e198d871e6e5ad5933426
ThomasN12 <phuocthinhdn12023@gmail.com>
2022-11-12 03:28:23 -0500

    add google login

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import Layout from './components/layout/Layout';
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/login/Login"
 import "bootstrap/dist/css/bootstrap.css";
+import TagList from './components/TagList';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 
 function App() {
   return (
     <Layout>
-      <Routes>
-        {/* <Route path="/" element={<HomePage />} /> */}
-        <Route path="/login" element={<Login />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
-        {/* <Route path="/" element={<HomePage />} />
-        <Route path="/campaigns" element={<AllCampaigns />} />
-        <Route path="/our-team" element={<EBoardMembers />} />
-        // <Route path="*" element={<NotFound />} /> */
-        <Route path='/' element={<Login/>}/>
-        }
-      </Routes>
+      <GoogleOAuthProvider clientId="868855841872-sc6bivb284l92isq1r9cude5fqhkt149.apps.googleusercontent.com">
+        <Routes>
+          {/* <Route path="/" element={<HomePage />} /> */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/tags" element={<TagList />} />
+          {/* <Route path="*" element={<NotFound />} /> */}
+          <Route path='/' element={<Login />} />
+        </Routes>
+      </GoogleOAuthProvider>
+
     </Layout>
 
   );
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,39 +1,69 @@
 import "./login.css"
 import React from "react"
+import { GoogleLogin } from '@react-oauth/google';
 
 const Login = (props) => {
+
+  const responseSuccessGoogle = (res) => {
+    const baseUrl = process.env.REACT_APP_ROOT_API;
+    // axios.post(`${baseUrl}/user/googlelogin`, { idToken: res.credential }).then(res => {
+    //   const { data } = res;
+    //   if (data.success)
+    //   {
+    //     localStorage.setItem('token', data.token);
+    //     navigate('/main');
+    //     toast.success(data.message);
+    //   } else
+    //   {
+    //     toast.error(data.message);
+    //   }
+    // }).catch(err => {
+    //   toast.error(err.message);
+    // })
+  }
+
+  const responseErrorGoogle = (err) => {
+    // toast.error(err.message);
+  }
   return (
-    <div className="Auth-form-container">
-      <form className="Auth-form">
-        <div className="Auth-form-content">
-          <h3 className="Auth-form-title">Sign In</h3>
-          <div className="form-group mt-3">
-            <label>Email address</label>
-            <input
-              type="email"
-              className="form-control mt-1"
-              placeholder="Enter email"
-            />
-          </div>
-          <div className="form-group mt-3">
-            <label>Password</label>
-            <input
-              type="password"
-              className="form-control mt-1"
-              placeholder="Enter password"
-            />
-          </div>
-          <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </div>
-          <p className="forgot-password text-right mt-2">
-            Forgot <a href="#">password?</a>
-          </p>
-        </div>
-      </form>
-    </div>
+    // <div className="Auth-form-container">
+    //   <form className="Auth-form">
+    //     <div className="Auth-form-content">
+    //       <h3 className="Auth-form-title">Sign In</h3>
+    //       <div className="form-group mt-3">
+    //         <label>Email address</label>
+    //         <input
+    //           type="email"
+    //           className="form-control mt-1"
+    //           placeholder="Enter email"
+    //         />
+    //       </div>
+    //       <div className="form-group mt-3">
+    //         <label>Password</label>
+    //         <input
+    //           type="password"
+    //           className="form-control mt-1"
+    //           placeholder="Enter password"
+    //         />
+    //       </div>
+    //       <div className="d-grid gap-2 mt-3">
+    //         <button type="submit" className="btn btn-primary">
+    //           Submit
+    //         </button>
+    //       </div>
+    //       <p className="forgot-password text-right mt-2">
+    //         Forgot <a href="#">password?</a>
+    //       </p>
+    //     </div>
+    //   </form>
+    // </div>
+    <GoogleLogin
+      buttonText="Login"
+      onSuccess={responseSuccessGoogle}
+      onFailure={responseErrorGoogle}
+      cookiePolicy={'single_host_origin'}
+      useOneTap
+    />
   )
 }
 
